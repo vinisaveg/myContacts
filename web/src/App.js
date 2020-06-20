1d6810f5f24cd958c6c8cc37780000a3443261c2
savegnago <savegdesigner@gmail.com>
2020-06-20 16:36:56 -0300

    web init

---
+++ web/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import './App.css'
+
+function App() {
+
+    return (
+
+        <div className="app">
+            <Header />
+        </div>
+
+    )
+
+}
+
+export default App
